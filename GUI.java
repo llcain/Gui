d8af2ecafc02b609b4af9993df01b43103a653e5
llcain <llcain2012@gmail.com>
2023-11-08 11:35:39 -0600

    Added background color to button

--- GUI.java
+++ GUI.java
@@ -1,13 +1,20 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI {
 
-    public static void main(String args[]) {
+    public GUI() {
         JFrame frame = new JFrame("My first Gui");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300,300);
         JButton button = new JButton("Press");
+        button.setBackground(Color.red);
         frame.getContentPane().add(button); // Adds Button to content pane of frame
         frame.setVisible(true);
+
+    }
+
+    public static void main(String args[]) {
+        new GUI();
     }
 }
